2de755c6fdda02d9e35ecdd2a29551239043af89
valinor <andresfullstack@gmail.com>
2021-03-21 14:56:47 -0300

    navbar & home responsive styles

--- src/components/HomeMainContent.js
+++ src/components/HomeMainContent.js
@@ -5,7 +5,7 @@ function homeMainContent() {
   return (
     <section className="main__content">
       <h1 className="main__content-title">
-        ¡Hola, somos <span>Gorilla Dev! </span>
+        ¡Hola, somos <span>Gorilla Dev! </span> <br />
         Diseñamos <span>experiencias</span>
       </h1>
       <div className="main__content-cards">
